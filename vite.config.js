a08360179e3954724abbbc485781a468e7260952
Kevin <kevindarmawan022@gmail.com>
2026-05-03 18:07:11 +0700

    feat: added assets dev support

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
+import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'core/resources/css/app.css',
+                'core/resources/js/app.js',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -14,11 +21,19 @@ export default defineConfig({
                 }),
             ],
         }),
+        vue(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '../core/resources/js'),
+        },
+    },
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        fs: {
+            allow: [
+                path.resolve(__dirname, '..'),
+            ],
         },
     },
 });
