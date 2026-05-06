f1f7e833b181d129571b68962dc0471a1c865147
Kevin <kevindrm0@gmail.com>
2026-05-06 14:52:13 +0700

    minor changes, broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -53,7 +53,6 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST'),
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
