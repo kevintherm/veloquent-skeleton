a72ba58e2f27858e16f5e3bf435e84da3f321622
Kevin <kevindrm0@gmail.com>
2026-05-10 19:03:33 +0700

    trust all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->remove(ConvertEmptyStringsToNull::class);
         $middleware->web([
             StartSession::class,
